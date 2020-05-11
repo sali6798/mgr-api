31a9ec08c9c00ffd5ec0a55cc7072040b5d18c10
sali6798 <shaidee@alingcastre.net>
2020-05-11 11:02:33 -0700

    added api route

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 const router = require("express").Router();
-// const apiRoutes = require("./api");
+const apiRoutes = require("./api");
 
 // API Routes
-// router.use("/api", apiRoutes);
+router.use("/api", apiRoutes);
 
 router.get("*", (req, res) => {
     res.send("welcome to my api!")
 })
 
 module.exports = router;
-
-
-// var allRoutes = require('./controllers');
