5543f0e123a4b082043ae55afb8e1d6626e6b2a9
bbtwots <brettbelka@gmail.com>
2020-05-11 15:35:03 -0700

    start routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
+const groupRoutes = require("./group");
+const postRoutes = require("./post");
 
 // User routes
 router.use("/user", userRoutes);
+
+router.use("/group", groupRoutes);
+
+router.use("/post", postRoutes);
 
 module.exports = router;
