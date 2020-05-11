be2a9b58804cb0eb9c23ee736b3b9558d88d05be
bbtwots <brettbelka@gmail.com>
2020-05-11 15:22:18 -0700

    controllers all set

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 const userRoutes = require("./user");
+const groupRoutes = require("./group");
+const postRoutes = require("./post");
 
 // User routes
 router.use("/user", userRoutes);
+
+//Group routes
+router.use("/group", groupRoutes);
+
+//Post routes
+router.use("/post", postRoutes);
 
 module.exports = router;
