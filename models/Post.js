cc18e714d7b9ce448d4c9f47eec587a6df5147d7
sali6798 <shaidee@alingcastre.net>
2020-05-16 13:39:01 -0700

    added event title

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+    eventTitle: {
+        type: String,
+        trim: true
+    },
     body: {
         type: String,
         trim: true
