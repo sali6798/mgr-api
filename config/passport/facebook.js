5c064b9500f3b434ff97e3782dc7f0bb50332519
bbtwots <brettbelka@gmail.com>
2020-05-18 14:25:40 -0700

    not much

--- config/passport/facebook.js
+++ config/passport/facebook.js
@@ -47,23 +47,23 @@ passport.use(
     )
 );
 
-// serialize the user.id to save in the cookie session
-// so the browser will remember the user when login
-passport.serializeUser((user, done) => {
-    console.log("SERIALIZE");
+// // serialize the user.id to save in the cookie session
+// // so the browser will remember the user when login
+// passport.serializeUser((user, done) => {
+//     console.log("SERIALIZE");
 
-    done(null, user.id);
-});
+//     done(null, user.id);
+// });
 
-// deserialize the cookieUserId to user in the database
-passport.deserializeUser((id, done) => {
-    console.log('DESERIALIZE');
+// // deserialize the cookieUserId to user in the database
+// passport.deserializeUser((id, done) => {
+//     console.log('DESERIALIZE');
 
-    User.findById(id)
-        .then(user => {
-            done(null, user);
-        })
-        .catch(e => {
-            done(new Error("Failed to deserialize a user"));
-        });
-});
+//     User.findById(id)
+//         .then(user => {
+//             done(null, user);
+//         })
+//         .catch(e => {
+//             done(new Error("Failed to deserialize a user"));
+//         });
+// });
