bec032dc60ec199233a2287cdc62031749ac0407
sali6798 <shaidee@alingcastre.net>
2020-05-19 15:36:48 -0700

    merge changes

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -23,11 +23,7 @@ router.post('/', (req, res, next) => {
                 return res.status(statusCode).json(error)
             }
 
-<<<<<<< HEAD
-            return res.json(user)
-=======
             return res.json(user);
->>>>>>> development
         })
     })(req, res, next);
 });
