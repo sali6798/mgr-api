5934dfee8af2e1451a72a94f8165b7ae7321be25
bbtwots <brettbelka@gmail.com>
2020-05-20 16:09:05 -0700

    twitter and facebook facebook redirect fix

--- routes/post/twitter.js
+++ routes/post/twitter.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const Twitter = require("twitter");
 require('dotenv').config();
-const fs = require("fs")
+const fs = require("fs");
+const path = require('path')
+
 
 
 router.post('/tweet', (req, res) => {
@@ -13,7 +15,7 @@ router.post('/tweet', (req, res) => {
         access_token_secret: req.user.twitterRefreshToken
     })
 
-    client.post("statuses/update", { status: "Oops, I tweeted." }, function (error, tweet, response) {
+    client.post("statuses/update", { status: "Yet Blah" }, function (error, tweet, response) {
         if (error) {
             console.log(error)
         } else {
@@ -26,29 +28,33 @@ router.post('/tweet', (req, res) => {
 
 router.post('/image', (req, res) => {
     const client = new Twitter({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
         access_token_key: req.user.twitterAccessToken,
         access_token_secret: req.user.twitterRefreshToken
     })
 
-    const imageData = fs.readFileSync("../../images/MV5BODIxMzU2NjY1Ml5BMl5BanBnXkFtZTcwMjcyMDEzMg@@._V1_SX300.jpg") //replace with the path to your image
+    const imageData = fs.readFileSync(path.join(__dirname, "quizPanda.png"))//replace with the path to your image
 
-    client.post("media/upload", { media: imageData }, function (error, media, response) {
+    client.post("media/upload", imageData, function (error, media, response) {
         if (error) {
             console.log(error)
         } else {
             const status = {
-                status: "I tweeted from Node.js!",
-                media_ids: media.media_id_string
+                status: "Random tweet with photo!",
+                media_ids: media.media_id_string,
+
             }
 
-            client.post("statuses/update", { status: "Oops, I tweeted again." }, function (error, tweet, response) {
+            // const status = {status:"This is a tweet"}
+
+            client.post("statuses/update", status, function (error, tweet, response) {
                 if (error) {
                     console.log(error)
                 } else {
                     console.log("Successfully tweeted an image!")
                 }
+                return res.json(response)
             })
         }
     })
