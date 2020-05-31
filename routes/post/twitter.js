a9b671989172f110de5d17c38bbc4021401fc124
bbtwots <brettbelka@gmail.com>
2020-05-30 22:39:37 -0700

    twitter file upload from url

--- routes/post/twitter.js
+++ routes/post/twitter.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const fs = require("fs");
 const path = require('path');
 const base64Img = require('base64-img');
+const https = require('https');
 
 
 router.post('/tweet', (req, res) => {
@@ -18,7 +19,7 @@ router.post('/tweet', (req, res) => {
     const text = req.body.text
 
 
-    client.post("statuses/update", { status:  text  }, function (error, tweet, response) {
+    client.post("statuses/update", { status: text }, function (error, tweet, response) {
         if (error) {
             console.log(error)
         } else {
@@ -29,64 +30,83 @@ router.post('/tweet', (req, res) => {
     })
 })
 
-router.post('/image', (req, res) => {
-    const client = new Twitter({
-        consumer_key: process.env.TWITTER_CONSUMER_KEY,
-        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-        access_token_key: req.user.twitterAccessToken,
-        access_token_secret: req.user.twitterRefreshToken
-    })
+// router.post('/image', (req, res) => {
+//     const client = new Twitter({
+//         consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//         consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//         access_token_key: req.user.twitterAccessToken,
+//         access_token_secret: req.user.twitterRefreshToken
+//     })
 
-    const imageData = fs.readFileSync(path.join(__dirname, "/Brett.jpg"))//replace with the path to your image
-    console.log(imageData);
+//     const imageData = fs.readFileSync(path.join(__dirname, "/Brett.jpg"))//replace with the path to your image
+//     console.log(imageData);
 
 
-    client.post("media/upload", imageData, function (error, media, response) {
-        console.log(error);
-        console.log(response);
-        if (error) {
-            console.log('1st error', error)
-        } else {
-            const status = {
-                status: "Random tweet with photo!",
-                media_ids: media.media_id_string,
+//     client.post("media/upload", imageData, function (error, media, response) {
+//         console.log(error);
+//         console.log(response);
 
-            }
+//         if (error) {
+//             console.log('1st error', error)
+//         } else {
+//             const status = {
+//                 status: "Random tweet with photo!",
+//                 media_ids: media.media_id_string,
+//             }
 
-            // const status = {status:"This is a tweet"}
+//             client.post("statuses/update", status, function (error, tweet, response) {
+//                 if (error) {
+//                     console.log('2nd error', error)
+//                 } else {
+//                     console.log("Successfully tweeted an image!")
+//                 }
+//                 return res.json(response)
+//             })
+//         }
+//     })
+// })
 
-            client.post("statuses/update", status, function (error, tweet, response) {
-                if (error) {
-                    console.log('2nd error', error)
+router.post('/media', function async(req, res) {
+    const dest = "./temp.jpg"
+    function download() {
+        console.log("download");
+
+        return new Promise((resolve, reject) => {
+
+            const file = fs.createWriteStream(dest, { flags: "wx" });
+
+            const request = https.get(req.body.url, response => {
+                if (response.statusCode === 200) {
+                    response.pipe(file);
                 } else {
-                    console.log("Successfully tweeted an image!")
+                    file.close();
+                    fs.unlink(dest, () => { }); // Delete temp file
+                    reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
                 }
-                return res.json(response)
-            })
-        }
-    })
-})
+            });
 
-router.post('/media', async (req, res) => {
+            request.on("error", err => {
+                file.close();
+                fs.unlink(dest, () => { }); // Delete temp file
+                reject("Bad file request", err.message);
+            });
 
-    // var url = 'http://res.cloudinary.com/mgr/image/upload/v1589655643/kwsv5tngyysrkcosi8su.png';
-    // base64Img.requestBase64(url, await function (err, res, body) {
-    //     console.log('_________err', err);
-    //     console.log('_________res', res.size);
-    //     // console.log('_________body.data:   ', body);
+            file.on("finish", () => {
+                resolve();
+            });
 
-    //     var src = body;
-    //     var mediaData = src.substr(22);
-    //     // console.log("mediaData", mediaData);
+            file.on("error", err => {
+                file.close();
 
-    //     let n = mediaData.length
-    //     console.log("FileSize: " + mediaData.length);
-
-    //     // where n is the length of base64 encoded string
-    //     var mediaSize = 4 * Math.ceil(2 * n / 3);
-    //     console.log('bytes', mediaSize);
-
-    // });
+                if (err.code === "EEXIST") {
+                    reject("File already exists");
+                } else {
+                    fs.unlink(dest, () => { }); // Delete temp file
+                    reject("File has errors", err.message);
+                }
+            });
+        });
+    }
 
     const client = new Twitter({
         consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -96,24 +116,29 @@ router.post('/media', async (req, res) => {
     })
 
     // const pathToFile = "http://res.cloudinary.com/mgr/image/upload/v1589655643/kwsv5tngyysrkcosi8su.png"
-    const mediaType = "image/jpg"
+    // const mediaType = "image/jpg"
 
-     const mediaData = fs.readFileSync(path.join(__dirname, "/Brett.jpg"))
+    // const mediaData = fs.readFileSync(path.join(__dirname, dest))
     // console.log(mediaData);
 
-    const mediaSize = fs.statSync(path.join(__dirname, "/Brett.jpg")).size
-    console.log(mediaSize);
+    // const mediaSize = fs.statSync(path.join(__dirname, dest)).size
+    // console.log(mediaSize);
 
-
-    initializeMediaUpload()
+    download()
+        .then(initializeMediaUpload)
         .then(appendFileChunk)
         .then(finalizeUpload)
+        .then(deleteTempFile)
         .then(publishStatusUpdate)
+
 
     function initializeMediaUpload() {
         console.log('init');
+        const mediaType = "image/jpg"
+        const mediaSize = fs.statSync(path.join(dest)).size
 
         return new Promise(function (resolve, reject) {
+
             client.post("media/upload", {
                 command: "INIT",
                 total_bytes: mediaSize,
@@ -132,7 +157,10 @@ router.post('/media', async (req, res) => {
     function appendFileChunk(mediaId) {
         console.log('append');
 
+        const mediaData = fs.readFileSync(path.join(dest))
+
         return new Promise(function (resolve, reject) {
+
             client.post("media/upload", {
                 command: "APPEND",
                 media_id: mediaId,
@@ -153,6 +181,7 @@ router.post('/media', async (req, res) => {
         console.log('finalize');
 
         return new Promise(function (resolve, reject) {
+
             client.post("media/upload", {
                 command: "FINALIZE",
                 media_id: mediaId
@@ -171,6 +200,7 @@ router.post('/media', async (req, res) => {
         console.log('publish');
 
         return new Promise(function (resolve, reject) {
+
             client.post("statuses/update", {
                 status: req.body.text,
                 media_ids: mediaId
@@ -187,6 +217,18 @@ router.post('/media', async (req, res) => {
             })
         })
     }
+
+    function deleteTempFile(path) {
+        console.log("delete");
+
+        fs.unlink(dest, (err) => {
+            if (err) {
+                console.log("failed to delete local image:" + err);
+            } else {
+                console.log('successfully deleted local image');
+            }
+        })
+    };
 })
 
 module.exports = router;
